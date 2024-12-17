225b33e0b50e5f9603574b61b1e495ed814e8fce
Igor Batista <igorbatista.dev@gmail.com>
2024-12-17 16:00:39 -0300

    Initial commit

---
+++ src/main/java/com/confetaria/confetaria_backend/ConfetariaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.confetaria.confetaria_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfetariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfetariaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/confetaria/confetaria_backend/ConfetariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confetaria.confetaria_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfetariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
